cdbcb5e9e0ced4b2f1890c71f766b91e978cc668
abyssmu <abyssmu@hotmail.com>
2022-07-06 11:54:09 -0500

    Obtained and cleaned a csv of all station data

--- Main.py
+++ Main.py
@@ -1,6 +1,57 @@
 import dataRequest as dR
+import requests
+import pandas as pd
 
-startDate = 2000
-endDate = 2019
+# startDate = 2000
+# endDate = 2019
 
-dR.requestBasics(startDate, endDate, 'data')
+# dR.requestBasics(startDate, endDate, 'data')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+### Downloading and cleaning the stations list
+
+# url = "https://www.ncei.noaa.gov/pub/data/ghcn/daily/ghcnd-stations.txt"
+# r = requests.get(url, allow_redirects = True)
+
+# open('stations.txt', 'wb').write(r.content)
+
+# stations = pd.read_csv('stations.txt', sep = " ", header = None)
+# print(stations)
+
+# states = ['AK', 'AL', 'AR', 'AS', 'AZ', 'CA', 'CO', 'CT', 'DC', 'DE',
+# 			'FL', 'GA', 'GU', 'HI', 'IA', 'ID', 'IL', 'IN', 'KS', 'KY',
+# 			'LA', 'MA', 'MD', 'ME', 'MI', 'MN', 'MO', 'MP', 'MS', 'MT',
+# 			'NC', 'ND', 'NE', 'NH', 'NJ', 'NM', 'NV', 'NY', 'OH', 'OK',
+# 			'OR', 'PA', 'PR', 'RI', 'SC', 'SD', 'TN', 'TX', 'UM', 'UT',
+# 			'VA', 'VI', 'VT', 'WA', 'WI', 'WV', 'WY',
+# 			'AB', 'BC', 'MB',
+# 			'NB', 'NL', 'NT', 'NS', 'NU', 'ON', 'PE', 'QC', 'SK', 'YT']
+
+# test = open('test.txt').readlines()
+# out = open('new.txt', 'w')
+
+# pos = 0
+
+# for i in range(len(test)):
+# 	# if len(test[i]) > 3:
+# 	# 	if test[i][pos] == " " and test[i][:2] in states:
+# 	# 		test[i] = test[i][:pos] + ',' + test[i][pos + 1:]
+
+# 	# 		print(test[i])
+	
+# 	test[i] = ',' + test[i]
+
+# 	out.write(test[i])
--- dataRequest.py
+++ dataRequest.py
@@ -1,6 +1,3 @@
-import datetime as d
-import json
-import os
 import pandas as pd
 import requests
 
@@ -51,39 +48,3 @@ def requestRange(startYear, endYear, datatype, fileName):
 		u = buildURL(startdate, enddate, datatype)
 
 		df[i] = pd.DataFrame(requests.get(u, headers={'token': token}).json()["results"])
-
-	to_excel(df, fileName)
-
-def to_excel(df, fileName):
-	f = fileName + '.xlsx'
-
-	if os.path.exists(f):
-		os.remove(f)
-
-	writer = pd.ExcelWriter(f, engine='xlsxwriter')
-
-	r = len(df)
-	for i in range(0, r):
-		df[i].to_excel(writer, sheet_name=('Sheet' + str(i)))
-
-	createChart(writer, r)
-
-	writer.save()
-
-def createChart(writer, r):
-	for i in range(0, r):
-		workbook = writer.book
-		worksheet = writer.sheets['Sheet' + str(i)]
-
-		chart = workbook.add_chart({'type': 'line'})
-
-		chart.add_series({
-			'categories': 'Sheet' + str(i) + '!$B$1:$B$366',
-			'values': 'Sheet' + str(i) + '!$F$1:$F$366',})
-
-		chart.set_x_axis({'name': 'Date', 'date_axis': True})
-		chart.set_y_axis({'name': 'Value', 'major_gridlines': {'visible': True}})
-		chart.set_legend({'position': 'none'})
-		chart.set_title({'name' : str(2000 + i)})
-
-		worksheet.insert_chart('H1', chart)
